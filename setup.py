81a873b3012428933c4048798c46194086493ce2
Jonathan Dursi <jonathan@dursi.ca>
2016-03-11 16:34:51 -0500

    Bump minor version number

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
 setup(
     name='mergevcf',
 
-    version='1.0.0',
+    version='1.0.1',
 
     description='Merge VCF calls',
     long_description=long_description,
